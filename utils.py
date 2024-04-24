06060b1b12bbf70ad7409a6a5c441cb25fc03bd8
Ilya Lunyov <ilunyov@bostonsd.ru>
2024-04-24 09:42:55 +0300

    feature: add utils.py

---
+++ utils.py
@@ -0,0 +1,22 @@
+import jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY = "just_secret_key_123"
+ALGORITHM = "HS256"
+EXPIRATION_TIME = timedelta(hours=1)
+
+
+def create_jwt_token(data: dict):
+    expiration = datetime.utcnow() + EXPIRATION_TIME
+    data.update({"exp": expiration})
+    token = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+    return token
+
+
+def verify_jwt_token(token: str):
+    try:
+        decoded_data = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return decoded_data
+    except jwt.PyJWTError:
+        return None
+
